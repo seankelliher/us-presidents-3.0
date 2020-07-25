43b810c3082ebe268befc8b6f2c4ec716cc2f0ad
Sean Kelliher <sbkelliher@icloud.com>
2020-07-24 23:56:57 -0400

    Add lists.js.

---
+++ lists.js
@@ -0,0 +1,70 @@
+//These arrays are used to create the "secondary navigation".
+const birthStateList = [
+    "New England",
+    "Mideast",
+    "Great Lakes",
+    "Plains",
+    "Southeast",
+    "Southwest",
+    "Rocky Mountain",
+    "Far West"
+];
+
+const partyAffiliationList = [
+    "Democratic",
+    "Democratic-Republican",
+    "Federalist",
+    "Republican",
+    "Whig",
+    "Unaffiliated"
+];
+
+const ageInauguratedList = [
+    "40 - 49",
+    "50 - 59",
+    "60 - 69",
+    "70 - 79"
+];
+
+const maritalStatusList = [
+    "Married",
+    "Never Married",
+    "Widower"
+];
+
+const previousJobGroupList = [
+    "Cabinet",
+    "Congress",
+    "Diplomatic",
+    "Governor",
+    "Military",
+    "Pres, VP",
+    "Private Industry"
+];
+
+const monthsInOfficeList = [
+    "less than 4 years",
+    "4 years",
+    "between 4 and 8 years",
+    "8 years",
+    "more than 8 years"
+];
+
+const reasonDepartureList = [
+    "Died",
+    "Lost Election",
+    "Not Nominated",
+    "Resigned",
+    "Retired",
+    "Term-Limited"
+];
+
+export {
+    birthStateList,
+    partyAffiliationList,
+    ageInauguratedList,
+    maritalStatusList,
+    previousJobGroupList,
+    monthsInOfficeList,
+    reasonDepartureList
+};
