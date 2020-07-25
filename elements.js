41b1c8f1adc1a0128eea930fa8c4dbadce33ad84
Sean Kelliher <sbkelliher@icloud.com>
2020-07-25 15:11:00 -0400

    Rename 'ulist' const to 'snl' const. More clarity.

--- elements.js
+++ elements.js
@@ -2,19 +2,19 @@
 //While loops  checks for firstChild, if true, removes lateChild.
 //This continues until condition if false (aka - all child elements removed).
 function clearSnl() {
-    const ulist = document.getElementById("snl");
-    while (ulist.firstChild) {
-        ulist.removeChild(ulist.lastChild);
+    const snl = document.getElementById("snl");
+    while (snl.firstChild) {
+        snl.removeChild(snl.lastChild);
     }
 }
 
 //Creates and appends "li" elements to list.
 function createSnl() {
     this.forEach(function (item) {
-        const ulist = document.getElementById("snl");
+        const snl = document.getElementById("snl");
         const litem = document.createElement("li");
         litem.textContent = item;
-        ulist.appendChild(litem);
+        snl.appendChild(litem);
     });
 }
 
