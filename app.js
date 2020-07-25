1b7525855ce770fbbe4048ce8b7e4c3ab5495c9a
Sean Kelliher <sbkelliher@icloud.com>
2020-07-25 16:57:36 -0400

    Add 'clearPnl' and 'selectPnl' functions. Modify function order for clarity.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {clearSnl, createSnl} from "./elements.js";
+import {clearPnl, clearSnl, selectPnl, createSnl} from "./elements.js";
 import {
     birthStateList,
     partyAffiliationList,
@@ -10,20 +10,28 @@ import {
 } from "./lists.js";
 
 document.getElementById("pnl").addEventListener("click", function (event) {
+    clearPnl();
     clearSnl();
     if (event.target.id === "birth") {
+        selectPnl(event.target);
         createSnl.apply(birthStateList);
     } else if (event.target.id === "party") {
+        selectPnl(event.target);
         createSnl.apply(partyAffiliationList);
     } else if (event.target.id === "age") {
+        selectPnl(event.target);
         createSnl.apply(ageInauguratedList);
     } else if (event.target.id === "marital") {
+        selectPnl(event.target);
         createSnl.apply(maritalStatusList);
     } else if (event.target.id === "previous") {
+        selectPnl(event.target);
         createSnl.apply(previousJobGroupList);
     } else if (event.target.id === "months") {
+        selectPnl(event.target);
         createSnl.apply(monthsInOfficeList);
     } else if (event.target.id === "departure") {
+        selectPnl(event.target);
         createSnl.apply(reasonDepartureList);
     }
 });
