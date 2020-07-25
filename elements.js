ee0173e0c2d72fdfba4d4e7ac4df4b8a40e3a004
Sean Kelliher <sbkelliher@icloud.com>
2020-07-25 14:19:01 -0400

    Add 'clearList' function and notes.

--- elements.js
+++ elements.js
@@ -1,3 +1,14 @@
+//Removes "li" elements from list.
+//While loops  checks for firstChild, if true, removes lateChild.
+//This continues until condition if false (aka - all child elements removed).
+function clearList() {
+    const ulist = document.getElementById("snl");
+    while (ulist.firstChild) {
+        ulist.removeChild(ulist.lastChild);
+    }
+}
+
+//Creates and appends "li" elements to list.
 function createList() {
     this.forEach(function (item) {
         const ulist = document.getElementById("snl");
@@ -7,4 +18,4 @@ function createList() {
     });
 }
 
-export {createList};
+export {clearList, createList};
