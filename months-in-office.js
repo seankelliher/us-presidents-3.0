40415340893e0257aa023d63013aadc6c894e417
Sean Kelliher <sbkelliher@icloud.com>
2020-07-26 12:17:58 -0400

    Modify names for 'consts' and 'exports'.

--- months-in-office.js
+++ months-in-office.js
@@ -1,31 +1,31 @@
 import allPresidents from "./presidents.js";
 
 //Placeholder variables for arrays.
-const lessthan48 = [];
-const exactly48 = [];
-const between48And96 = [];
-const exactly96 = [];
-const moreThan96 = [];
+const lessThan4 = [];
+const exactly4 = [];
+const between4And8 = [];
+const exactly8 = [];
+const moreThan8 = [];
 
 //Sort all presidents into "age inaugurated" arrays.
 allPresidents.forEach(function (index) {
     if (index.monthsInOffice < 48) {
-        lessthan48.push(index.number);
+        lessThan4.push(index.number);
     } else if (index.monthsInOffice === 48) {
-        exactly48.push(index.number);
+        exactly4.push(index.number);
     } else if ((index.monthsInOffice >= 49) && (index.monthsInOffice < 96)) {
-        between48And96.push(index.number);
+        between4And8.push(index.number);
     } else if (index.monthsInOffice === 96) {
-        exactly96.push(index.number);
+        exactly8.push(index.number);
     } else if (index.monthsInOffice > 96) {
-        moreThan96.push(index.number);
+        moreThan8.push(index.number);
     }
 });
 
 export {
-    lessthan48,
-    exactly48,
-    between48And96,
-    exactly96,
-    moreThan96
+    lessThan4,
+    exactly4,
+    between4And8,
+    exactly8,
+    moreThan8
 };
