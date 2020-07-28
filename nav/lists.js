95c0a81d168dac3d007ef77efc6b284354bac7e4
Sean Kelliher <sbkelliher@icloud.com>
2020-07-27 23:58:28 -0400

    Move 'lists.js' into 'nav' folder.

---
+++ nav/lists.js
@@ -0,0 +1,70 @@
+//These arrays are used to create the "secondary navigation".
+const birthStateList = [
+    "New England",
+    "Mideast",
+    "Great Lakes",
+    "Plains",
+    "Southeast",
+    "Southwest",
+    "Rocky Mountain",
+    "Far West"
+];
+
+const partyAffiliationList = [
+    "Democratic",
+    "Democratic-Republican",
+    "Federalist",
+    "Republican",
+    "Whig",
+    "Unaffiliated"
+];
+
+const ageInauguratedList = [
+    "Forties",
+    "Fifites",
+    "Sixties",
+    "Seventies"
+];
+
+const maritalStatusList = [
+    "Married",
+    "Never Married",
+    "Widower"
+];
+
+const previousJobGroupList = [
+    "Cabinet",
+    "Congress",
+    "Diplomatic",
+    "Governor",
+    "Military",
+    "Pres, VP",
+    "Private Industry"
+];
+
+const timeInOfficeList = [
+    "less than 4",
+    "exactly 4",
+    "between 4 and 8",
+    "exactly 8",
+    "more than 8"
+];
+
+const reasonDepartureList = [
+    "Died",
+    "Lost Election",
+    "Not Nominated",
+    "Resigned",
+    "Retired",
+    "Term-Limited"
+];
+
+export {
+    birthStateList,
+    partyAffiliationList,
+    ageInauguratedList,
+    maritalStatusList,
+    previousJobGroupList,
+    timeInOfficeList,
+    reasonDepartureList
+};
