2f713aa2d4419f528babb6143cc228cec6d12874
Sean Kelliher <sbkelliher@icloud.com>
2020-07-29 23:07:57 -0400

    Rename 'createSnl' to 'createNavList' function for clarity. Rework it too.

--- nav/elements.js
+++ nav/elements.js
@@ -26,17 +26,17 @@ function highlightPnl(item) {
         item.classList.add("selected");
 }
 
-//Creates and appends "li" items to the "secondary" nav list.
-function createSnl() {
+//Creates and appends "li" items to the "primary" or secondary" nav lists.
+function createNavList(navId) {
     this.forEach(function (item) {
-        const snl = document.getElementById("snl");
-        const snlListItem = document.createElement("li");
-        snlListItem.textContent = item;
+        const navList = document.getElementById(navId);
+        const navListItem = document.createElement("li");
+        navListItem.textContent = item;
         const lowerCase = item.toLowerCase();
         const removeSpace = /\s|,\s/g;
         const addDash = lowerCase.replace(removeSpace, "-");
-        snlListItem.id = addDash;
-        snl.appendChild(snlListItem);
+        navListItem.id = addDash;
+        navList.appendChild(navListItem);
     });
 }
 
