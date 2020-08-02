3818b8661dd0a80abbc82fbc49152d21585e7225
Sean Kelliher <sbkelliher@icloud.com>
2020-08-01 20:37:52 -0400

    Move lists.js out of 'nav' folder.

---
+++ lists.js
@@ -0,0 +1,82 @@
+//This array is used to create the "primary navigation".
+const primaryNav = [
+    "Birthplace",
+    "Party Affiliation",
+    "Age When Inaugurated",
+    "Marital Status",
+    "Previous Job Type",
+    "Years as President",
+    "Reason for Departure"
+];
+
+//These arrays are used to create the "secondary navigation".
+const birthplaceNav = [
+    "New England",
+    "Mideast",
+    "Great Lakes",
+    "Plains",
+    "Southeast",
+    "Southwest",
+    "Rocky Mountain",
+    "Far West"
+];
+
+const partyAffiliationNav = [
+    "Democratic",
+    "Democratic-Republican",
+    "Federalist",
+    "Republican",
+    "Whig",
+    "Unaffiliated"
+];
+
+const ageWhenInauguratedNav = [
+    "Forties",
+    "Fifites",
+    "Sixties",
+    "Seventies"
+];
+
+const maritalStatusNav = [
+    "Married",
+    "Never Married",
+    "Widower"
+];
+
+const previousJobTypeNav = [
+    "Cabinet",
+    "Congress",
+    "Diplomatic",
+    "Governor",
+    "Military",
+    "Pres, VP",
+    "Private Industry"
+];
+
+const yearsAsPresidentNav = [
+    "less than 4",
+    "exactly 4",
+    "between 4 and 8",
+    "exactly 8",
+    "more than 8"
+];
+
+const reasonForDepartureNav = [
+    "Died",
+    "Lost Election",
+    "Not Nominated",
+    "Resigned",
+    "Retired",
+    "Term-Limited"
+];
+
+export {
+    primaryNav,
+    birthplaceNav,
+    partyAffiliationNav,
+    ageWhenInauguratedNav,
+    maritalStatusNav,
+    previousJobTypeNav,
+    yearsAsPresidentNav,
+    reasonForDepartureNav
+};
