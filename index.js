130b3bc0544e7072e762937d108573bf07f55ed0
Sean Kelliher <sbkelliher@icloud.com>
2020-12-24 15:08:05 -0500

    Add calls for 'highlightListItem' function. Add notes, tidy code too.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {clearSecondaryNav, chooseSecondaryNav} from "./app/nav.js";
+import {clearSecondaryNav, chooseSecondaryNav, highlightListItem} from "./app/nav.js";
 import {clearGallery, createGallery} from "./app/badges.js";
 import {filterGallery} from "./app/filters.js";
 import {hideEmpty} from "./app/empty.js";
@@ -6,20 +6,34 @@ import {hideEmpty} from "./app/empty.js";
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
-    //Loads "birthplace" as default secondary nav.
+    //Retrieve lists and item.
+    const primaryList = document.getElementById("primary-list");
+    const secondaryList = document.getElementById("secondary-list");
+    const birthplace = document.getElementById("birthplace");
+
+    //When page loads, highlight "birthplace" in primary list.
+    highlightListItem(primaryList, birthplace);
+
+    //Create "birthplace" list in secondary nav.
     chooseSecondaryNav("birthplace");
+
+    //Create "birthplace" items in gallery.
     createGallery("birthplace");
+
+    //Invoke event listeners functions.
     filterGallery();
     hideEmpty();
 
-    //Retrieve primary list.
-    const primaryList = document.getElementById("primary-list");
-
-    //Add event listener.
+    //Monitor lists for user clicks.
     primaryList.addEventListener("click", function (event) {
         clearSecondaryNav();
         chooseSecondaryNav(event.target.id);
         clearGallery();
         createGallery(event.target.id);
+        highlightListItem(primaryList, event.target);
+    });
+
+    secondaryList.addEventListener("click", function (event) {
+        highlightListItem(secondaryList, event.target);
     });
 });
