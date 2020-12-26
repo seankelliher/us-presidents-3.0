f18c50200e377c712f682f9f9c9ef15e7c29f176
Sean Kelliher <sbkelliher@icloud.com>
2020-12-25 22:01:41 -0500

    Add notes. Remove extra 'const emptyMsg.

--- app/empty.js
+++ app/empty.js
@@ -1,3 +1,4 @@
+//Hides "empty" message.
 function hideEmpty() {
     "use strict";
 
@@ -5,29 +6,40 @@ function hideEmpty() {
     emptyMsg.style.display = "none";
 }
 
-
+//Checks if no presidents match list items user clicks.
+//Displays "empty" message, if needed.
 function checkEmpty() {
     "use strict";
 
+    //Gather all president divs.
     const presidents = document.getElementsByClassName("president");
+
+    //Convert to "real" array. (works without doing too).
     const presidentsArray = Array.from(presidents);
 
+    //Collection array.
     const collection = [];
 
+    //Loop through president divs.
+    //If div is hidden, add "none" to collection array (above).
     presidentsArray.forEach(function (president) {
         if (president.style.display === "none") {
             collection.push("none");
         }
     });
 
+    //If all presidents are hidden, display "empty" message.
+    //NOTE: adjust this number, when more presidents added to component.
+    const emptyMsg = document.getElementById("empty-msg");
+
     if (collection.length === 45) {
-        const emptyMsg = document.getElementById("empty-msg");
         emptyMsg.style.display = "block";
     }
 
+    //If all presidents are not hidden, hide "empty" message.
     if (collection.length !== 45) {
-        const emptyMsg = document.getElementById("empty-msg");
         emptyMsg.style.display = "none";
     }
 }
-export{hideEmpty, checkEmpty};
+
+export {hideEmpty, checkEmpty};
