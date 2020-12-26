f0344f0ca1d63b1ad1110acdfefc622132ca8776
Sean Kelliher <sbkelliher@icloud.com>
2020-12-25 23:00:07 -0500

    Add notes.

--- app/filters.js
+++ app/filters.js
@@ -1,12 +1,20 @@
+//This module watches secondary list for clicks.
+//Shows and hides president divs in gallery.
+
 import {checkEmpty} from "./empty.js";
 
 function filterGallery() {
     "use strict";
 
+    //Get the secondary list.
     const secondaryList = document.getElementById("secondary-list");
 
+    //Add event listener. When user clicks...
     secondaryList.addEventListener("click", function (event) {
 
+        //Get the target list item's text content.
+        //Replace space with dash. Make lower case.
+        //For example: New England -> new-england.
         const etc = event.target.textContent;
         const regex = /\s/g;
         const converted = etc.replace(regex, "-").toLowerCase();
@@ -18,6 +26,10 @@ function filterGallery() {
         const leftParasArray = Array.from(leftParas);
 
         //Loop through "p" tags.
+        //If "p" tag's class name === li's converted text content,
+        //For example: new-england === new-england.
+        //Get "president" div and display it (flex).
+        //If not, get "president" div and hide it.
         leftParasArray.forEach(function (para) {
             if (para.className === converted) {
                 para.parentElement.parentElement.style.display = "flex";
@@ -25,7 +37,7 @@ function filterGallery() {
                 para.parentElement.parentElement.style.display = "none";
             }
         });
-        checkEmpty(); //testing
+        checkEmpty();
     });
 }
 
